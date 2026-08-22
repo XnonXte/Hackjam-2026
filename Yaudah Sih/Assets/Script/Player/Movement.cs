1c928ed6de9f673d1df91d3523763448827ec29c
nadhifaiz <m.faiznadhif.r@gmail.com>
2026-08-22 13:02:21 +0700

    Initial commit
    Create Unity Project

---
+++ Yaudah Sih/Assets/Script/Player/Movement.cs
@@ -0,0 +1,86 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Movement : MonoBehaviour
+{
+    [Header("Movement")]
+    [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private float jumpForce = 10f;
+    [SerializeField] private InputActionReference moveAction;
+    [SerializeField] private InputActionReference jumpAction;
+
+    [Header("Ground Check")]
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private float groundCheckRadius = 0.15f;
+    [SerializeField] private LayerMask groundLayer;
+
+    private Rigidbody2D rb;
+    private float horizontalInput;
+    private bool jumpRequested;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void OnEnable()
+    {
+        moveAction?.action.Enable();
+        jumpAction?.action.Enable();
+    }
+
+    private void OnDisable()
+    {
+        moveAction?.action.Disable();
+        jumpAction?.action.Disable();
+    }
+
+    private void Update()
+    {
+        horizontalInput = moveAction == null
+            ? 0f
+            : moveAction.action.ReadValue<Vector2>().x;
+
+        if (jumpAction != null && jumpAction.action.WasPressedThisFrame())
+        {
+            jumpRequested = true;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        rb.linearVelocity = new Vector2(horizontalInput * moveSpeed, rb.linearVelocity.y);
+
+        if (jumpRequested && IsGrounded())
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+        }
+
+        jumpRequested = false;
+    }
+
+    private bool IsGrounded()
+    {
+        if (groundCheck == null)
+        {
+            return rb.IsTouchingLayers(groundLayer);
+        }
+
+        return Physics2D.OverlapCircle(
+            groundCheck.position,
+            groundCheckRadius,
+            groundLayer) != null;
+    }
+
+    // private void OnDrawGizmosSelected()
+    // {
+    //     if (groundCheck == null)
+    //     {
+    //         return;
+    //     }
+    // }
+    // Gizmos.color = Color.yellow;
+    //     Gizmos.DrawWireSphere(groundCheck.position, groundCheckRadius);
+
+}
